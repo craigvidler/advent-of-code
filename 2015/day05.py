7a45865b8ab0b0469ad88856bfa1db87633491aa
Craig Vidler <craig@craigvidler.co.uk>
2024-07-22 22:02:29 +0100

    Corrected typo in 2015 Day 5

--- 2015/day05.py
+++ 2015/day05.py
@@ -15,7 +15,7 @@ def part1(words):
     return sum(
         all((
             len([c for c in word if c in 'aeiou']) >= 3,
-            any(a == b for a, b, in pairwise(word)),
+            any(a == b for a, b in pairwise(word)),
             all(a + b not in ['ab', 'cd', 'pq', 'xy'] for a, b in pairwise(word))
         ))
         for word in words
