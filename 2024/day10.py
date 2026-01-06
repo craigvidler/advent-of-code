e82db0ddf8a0e9220672dcd4955c3d29b0b0adb9
Craig Vidler <craig@craigvidler.co.uk>
2026-01-06 05:21:16 +0000

    2024, Day 10, finished version.

--- 2024/day10.py
+++ 2024/day10.py
@@ -6,65 +6,50 @@ https://adventofcode.com/2024/day/10
 from aocd import data
 
 
-def neighbors(r, c, grid):
+def parse(data):
+    return [[int(c) for c in line] for line in data.splitlines()]
+
+
+def neighbors(r, c):
     for nr, nc in (r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1):
         if 0 <= nr < MAXROW and 0 <= nc < MAXCOL:
             yield nr, nc
 
 
-def get_heads(grid):
+def trailheads(grid):
     for r, row in enumerate(grid):
         for c, col in enumerate(row):
             if col == 0:
                 yield r, c
 
 
-def dfs(grid, r, c, peaks):
-    if ((r, c)) in peaks:
-        return
-
+def dfs(grid, r, c):
     if grid[r][c] == 9:
-        peaks.add((r, c))
+        return [(r, c)]  # using coords of each 9 as unique id
 
-    for nr, nc in neighbors(r, c, grid):
+    peaks_reached = []
+    for nr, nc in neighbors(r, c):
         if grid[nr][nc] == grid[r][c] + 1:
-            dfs(grid, nr, nc, peaks)
+            peaks_reached.extend(dfs(grid, nr, nc))
 
-    return peaks
-
-
-def p2_dfs(grid, r, c, count):
-    if grid[r][c] == 9:
-        count += 1
-
-    for nr, nc in neighbors(r, c, grid):
-        if grid[nr][nc] == grid[r][c] + 1:
-            count = p2_dfs(grid, nr, nc, count)
-
-    return count
+    return peaks_reached
 
 
-def parse(data):
-    return [[int(c) for c in line] for line in data.splitlines()]
+def walk(grid):
+    distinct_peaks = total_paths = 0
 
+    for r, c in trailheads(grid):
+        peaks_reached = dfs(grid, r, c)
+        distinct_peaks += len(set(peaks_reached))
+        total_paths += len(peaks_reached)
 
-def part1(grid):
-    total = 0
-    for r, c in get_heads(grid):
-        peaks = set()
-        total += len(dfs(grid, r, c, peaks))
-    return total
-
-
-def part2(grid):
-    total = count = 0
-    for r, c in get_heads(grid):
-        total += p2_dfs(grid, r, c, count)
-    return total
+    return distinct_peaks, total_paths
 
 
 if __name__ == "__main__":
     grid = parse(data)
+    # For now. Don't like it but don't like alternatives either.
     MAXROW, MAXCOL = len(grid), len(grid[0])
-    print("Part 1:", part1(grid))
-    print("Part 2:", part2(grid))
+    score, rating = walk(grid)
+    print("Part 1:", score)
+    print("Part 2:", rating)
