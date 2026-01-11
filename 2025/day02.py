2aaf2c43fd9090eb26133aa59253c7cb9385efbe
Craig Vidler <craig@craigvidler.co.uk>
2026-01-11 17:39:26 +0000

    2025, Day 2. Tightened up.

--- 2025/day02.py
+++ 2025/day02.py
@@ -5,7 +5,6 @@ https://adventofcode.com/2025/day/2
 
 import re
 
-import parse as p
 from aocd import data
 
 P1REGEX = re.compile(r"(\d+)\1")
@@ -13,17 +12,14 @@ P2REGEX = re.compile(r"(\d+)\1+")  # bosh
 
 
 def parse(data):
-    pattern = p.compile("{:d}-{:d}")
-    return [pattern.parse(pair) for pair in data.split(",")]
+    return [
+        range(int(start), int(end) + 1)
+        for start, end in [pair.split("-") for pair in data.split(",")]
+    ]
 
 
 def run(ranges, regex):
-    total = 0
-    for start, end in ranges:
-        for n in range(start, end + 1):
-            if m := regex.fullmatch(str(n)):
-                total += int(m.group(0))
-    return total
+    return sum(n for r in ranges for n in r if regex.fullmatch(str(n)))
 
 
 if __name__ == "__main__":
