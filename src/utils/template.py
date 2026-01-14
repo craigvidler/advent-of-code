9378fd48e459117f1d9b764223b4dba1754a640c
Craig Vidler <craig@craigvidler.co.uk>
2026-01-14 05:27:48 +0000

    Changed project structure (src-based pattern now), added pyproject.toml and utils folder - all in preparation for `pip install -e .` and `from utils.grid import Grid` etc.

---
+++ src/utils/template.py
@@ -0,0 +1,20 @@
+from aocd import data
+
+
+def parse(data):
+    return data
+
+
+def part1(lines):
+    pass
+
+
+def part2(lines):
+    pass
+
+
+if __name__ == '__main__':
+    parsed = parse(data)
+    print(parsed)
+    print('Part 1:', part1(parsed))
+    print('Part 2:', part2(parsed))
